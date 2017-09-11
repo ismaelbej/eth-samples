1e22b0df0b7140a517a4d5ff16a7632c07035b01
Ismael Bejarano <ismaelbej@gmail.com>
2017-09-11 00:38:47 -0300

    Try with array of bytes32

--- arrays/contracts/MetaCoin.sol
+++ arrays/contracts/MetaCoin.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.4;
 
 contract MetaCoin {
-    uint[] public test;
+    bytes32[] public test;
 
-    function init(uint[] _test) public returns (bool) {
+    function init(bytes32[] _test) public returns (bool) {
         for (uint i=0; i<_test.length; ++i) {
             if (i >= test.length) {
                 test.push(_test[i]);
