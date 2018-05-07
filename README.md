539149de089ba91dfbdb0c9b5c0259c3617f47d3
Ismael Bejarano <ismaelbej@gmail.com>
2018-05-07 02:19:06 -0300

    How to parse tx params with ethereumjs-abi

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Samples for ethereum, solidity, truffle, etc.
 
 * calc_address.js: Calculate address from private key
 * compile_contract.js: Compile a simple contract using solc
+* parse_parameters.js: Parse tx parameters using ethereumjs-abi
 * web3_v1.0: Deploy and query contract using web3 v1.0
---
+++ parse_parameters.js
@@ -0,0 +1,21 @@
+const abi = require('ethereumjs-abi');
+
+function parseTxParams() {
+  const data = Buffer.from(`000000000000000000000000b56d622ddf60ec532b5f43b4ff9b0e7b1ff92db30000000000000000000000000000000000000000000000015af1d78b58c4000000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000`, 'hex');
+
+  const decoded = abi.rawDecode(['address', 'uint256', 'bytes'], data);
+
+  console.log(`Decoded: ${JSON.stringify(decoded, null, '  ')}`);
+
+  const params = [
+    '0xb56d622ddf60ec532b5f43b4ff9b0e7b1ff92db3',
+    '0x15af1d78b58c40000',
+    []
+  ];
+
+  const encoded = abi.rawEncode(['address', 'uint256', 'bytes'], params);
+
+  console.log(`Encoded: ${encoded.toString('hex')}`);
+}
+
+parseTxParams();
