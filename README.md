b7dbc49197f5c7ae7ce1169a75fd016b31d08b33
Ismael Bejarano <ismaelbej@gmail.com>
2018-05-10 01:23:31 -0300

    Update multiple transactions to use web3 v1

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Samples for ethereum, solidity, truffle, etc.
 
 * calc_address.js: Calculate address from private key
 * compile_contract.js: Compile a simple contract using solc
+* multiple_transactions.js: Create multiple transactions
 * parse_parameters.js: Parse tx parameters using ethereumjs-abi
 * web3_v1.0: Deploy and query contract using web3 v1.0
---
+++ multiple_transactions.js
@@ -0,0 +1,39 @@
+const Web3 = require('web3');
+
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+const eth = web3.eth;
+
+async function sendMultipleTransactions() {
+
+  const accounts = await eth.personal.getAccounts();
+
+  const account0 = accounts[0];
+  const account1 = '0x583031d1113ad414f02576bd6afabfb302140225';
+
+  const start = await eth.getTransactionCount(account0);
+
+  console.log(`Account: ${account0}`);
+  console.log(`Start: ${start}`);
+
+  const arr = [];
+  for (let i=0; i<500; ++i) {
+    const hash = await new Promise((resolve, reject) => {
+      eth.sendTransaction({
+        from: account0,
+        to: account1,
+        nonce: start + i,
+        value: web3.utils.toWei((i/1000+1).toFixed(4), 'szabo')
+      })
+      .on('transactionHash', (hash) => {
+        resolve(hash);
+      });
+    });
+    arr.push(hash);
+  }
+
+  console.log(`First nonce: ${(await eth.getTransaction(arr[0])).nonce}`);
+  console.log(`Last nonce: ${(await eth.getTransaction(arr[499])).nonce}`);
+}
+
+sendMultipleTransactions();
