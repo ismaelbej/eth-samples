e42a15bd89ab4cd83aed1083c92294062feed313
Ismael Bejarano <ismaelbej@gmail.com>
2019-03-03 01:07:31 -0300

    Convert uint256 two's complement example to unit test

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Samples for ethereum, solidity, truffle, etc.
 * multiple_transactions.js: Create multiple transactions
 * parse_parameters.js: Parse tx parameters using ethereumjs-abi
 * sharing_struct/: Sharing struct definition between two contracts
+* sign_transaction.js: Sign transaction with ethereumjs-tx
+* uint_two_complement.js: Two's complement uint256 in js
---
+++ uint_two_complement.js
@@ -0,0 +1,18 @@
+const test = require('tape');
+const BN = require('bn.js');
+
+test('Uint two\'s complement', async (t) => {
+  const a = new BN(1);
+  const b = new BN(-1);
+
+  const c = a.toTwos(256);
+  const d = b.toTwos(256);
+
+  t.equal(c.toString(16, 64), '0000000000000000000000000000000000000000000000000000000000000001', 'Positive two\'s complement');
+  t.equal(d.toString(16, 64), 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 'Negative two\'s complement');
+
+  console.log(`A: ${c.toString(16, 64)}`);
+  console.log(`B: ${d.toString(16, 64)}`);
+
+  t.end();
+});
