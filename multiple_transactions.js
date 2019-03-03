0d9d1bed51615c69e8d071403da6093231ec0e24
Ismael Bejarano <ismaelbej@gmail.com>
2019-03-02 23:41:47 -0300

    Multiple transactions as unit test

--- multiple_transactions.js
+++ multiple_transactions.js
@@ -1,28 +1,26 @@
-const Web3 = require('web3');
+const test = require('tape');
+const utils = require('./utils');
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const NUM_TRANSACTIONS = 500;
 
-const eth = web3.eth;
-
-async function sendMultipleTransactions() {
-
-  const accounts = await eth.personal.getAccounts();
+test('Send many transactions', async (t) => {
+  const { web3, accounts } = await utils.initChain();
 
   const account0 = accounts[0];
-  const account1 = '0x583031d1113ad414f02576bd6afabfb302140225';
+  const account1 = accounts[1];
 
-  const start = await eth.getTransactionCount(account0);
+  const start = await web3.eth.getTransactionCount(account0);
 
   console.log(`Account: ${account0}`);
   console.log(`Start: ${start}`);
 
   const arr = [];
-  for (let i=0; i<500; ++i) {
+  for (let i=0; i<NUM_TRANSACTIONS; ++i) {
     const hash = await new Promise((resolve, reject) => {
-      eth.sendTransaction({
+      web3.eth.sendTransaction({
         from: account0,
         to: account1,
-        nonce: start + i,
+        nonce: web3.utils.toHex(start + i),
         value: web3.utils.toWei((i/1000+1).toFixed(4), 'szabo')
       })
       .on('transactionHash', (hash) => {
@@ -32,8 +30,14 @@ async function sendMultipleTransactions() {
     arr.push(hash);
   }
 
-  console.log(`First nonce: ${(await eth.getTransaction(arr[0])).nonce}`);
-  console.log(`Last nonce: ${(await eth.getTransaction(arr[499])).nonce}`);
-}
+  const first = (await web3.eth.getTransaction(arr[0])).nonce;
+  const last = (await web3.eth.getTransaction(arr[arr.length-1])).nonce;
 
-// sendMultipleTransactions();
+  console.log(`First nonce: ${first}`);
+  console.log(`Last nonce: ${last}`);
+
+  t.equal(first, start, 'First nonce should match');
+  t.equal(last, start + NUM_TRANSACTIONS - 1, 'Last nonce should match');
+
+  t.end();
+});
