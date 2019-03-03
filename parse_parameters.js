72a87d6c8ba216d7943d5c7f55a29852e047376b
Ismael Bejarano <ismaelbej@gmail.com>
2019-03-03 00:14:29 -0300

    Convert parse_parameters to unit test

--- parse_parameters.js
+++ parse_parameters.js
@@ -1,9 +1,17 @@
+const test = require('tape');
 const abi = require('ethereumjs-abi');
 
-function parseTxParams() {
+test('Parse tx input data', async (t) => {
   const data = Buffer.from(`000000000000000000000000b56d622ddf60ec532b5f43b4ff9b0e7b1ff92db30000000000000000000000000000000000000000000000015af1d78b58c4000000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000000`, 'hex');
 
   const decoded = abi.rawDecode(['address', 'uint256', 'bytes'], data);
+
+  t.ok(decoded, 'Decoded input data');
+  t.equal(decoded.length, 3, 'Decoded three parameters');
+
+  t.equal(decoded[0], 'b56d622ddf60ec532b5f43b4ff9b0e7b1ff92db3', 'Address parameter');
+  t.equal(decoded[1].toString(16), '15af1d78b58c40000', 'Uint256 parameter');
+  t.equal(decoded[2].length, 0, 'Bytes array is empty');
 
   console.log(`Decoded: ${JSON.stringify(decoded, null, '  ')}`);
 
@@ -15,7 +23,10 @@ function parseTxParams() {
 
   const encoded = abi.rawEncode(['address', 'uint256', 'bytes'], params);
 
-  console.log(`Encoded: ${encoded.toString('hex')}`);
-}
+  t.ok(encoded, 'Encoded parameters');
+  t.equal(encoded.length, 128, 'Encoded is 128 bytes');
 
-// parseTxParams();
+  console.log(`Encoded: ${encoded.toString('hex')}`);
+
+  t.end();
+});
