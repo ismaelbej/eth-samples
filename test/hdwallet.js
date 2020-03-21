c93acea79a13e42f447c422c197d28030b59c16e
Ismael Bejarano <ismaelbej@gmail.com>
2020-03-21 19:32:43 -0300

    Move hdwallet to a proper test

---
+++ test/hdwallet.js
@@ -0,0 +1,20 @@
+const test = require('tape');
+const bip39 = require('bip39');
+const hdkey = require('ethereumjs-wallet/hdkey');
+
+test('Compile, deploy and query contract', async (t) => {
+    const mnemonic = 'party box feel talent peace tiger endorse defy cheese girl tumble mail';
+    const seed = await bip39.mnemonicToSeed(mnemonic);
+    const path = "m/44'/60'/0'/0/0";
+
+    const hdwallet = hdkey.fromMasterSeed(seed);
+    const myWallet = hdwallet.derivePath("m/44'/60'/0'/0/0").getWallet();
+
+    const address = myWallet.getAddressString();
+    //console.log(`Address: ${address}`);
+    //console.log(`Private Key: ${myWallet.getPrivateKeyString()}`)
+
+    t.equal(address, "0x999c43b9dd689b9a0bf12ce097663857f62ffbe7", "Address");
+
+    t.end();
+});
