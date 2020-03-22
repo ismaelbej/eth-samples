5c1f9be79f81c96e731261a4a3fcc5d8f327f5b0
Ismael Bejarano <ismaelbej@gmail.com>
2020-03-22 00:27:39 -0300

    Remove unused test

--- arrays/migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations, { from: "0x7fe01972a2bf4840ba358ce516defa9b20ad92d3" });
-};
