486b69eb2b2ad3d1386f044ceb2e377705276ae7
Ismael Bejarano <ismaelbej@gmail.com>
2021-01-09 00:18:01 -0300

    Fix transaction signature

--- test/signTransaction.js
+++ test/signTransaction.js
@@ -1,36 +1,41 @@
 const test = require('tape');
-const Transaction = require('ethereumjs-tx').Transaction;
+const { Transaction } = require('@ethereumjs/tx');
+const { default: Common } = require('@ethereumjs/common');
 
 test('Sign transaction', async (t) => {
 
-  const address = 'faab41ed7141e4939a516e499e5a8b6b5163144d';
+  const address = '0xfaab41ed7141e4939a516e499e5a8b6b5163144d';
   const pk = Buffer.from('f53a7a5247380eabb52f021e14a67815ce6b6966171a12d6e9533aa37364fa8f', 'hex');
+
+  const common = Common.forCustomChain(
+    'mainnet',
+    {
+      networkId: 231,
+      chainId: 231,
+    },
+    'petersburg'
+  );
 
   const txData = {
     nonce: '0x11',
     gasPrice: '0x04a817c800',
-    gasLimit: '0x5208',
+    gasLimit: '0x52d4',
     to: '0x31483bb3cae99bf173e5f61f0c62dc398f197b81',
     value: '0x03e8',
-    data: '0xc0ffee',
-    chainId: 231
+    data: '0xc0ffee'
   };
 
-  const tx = new Transaction(txData);
+  let tx = Transaction.fromTxData(txData, { common });
   t.ok(tx, 'Build transaction');
-  t.equal(tx.r.length, 0, 'Unsigned transaction');
-
-  tx.sign(pk);
-  t.equal(tx.r.length, 32, 'Signed transaction');
+  tx = tx.sign(pk);
+  t.ok(tx.validate(), 'Valid transaction');
 
   const serializedTx = tx.serialize();
 
-  //console.log(`Tx: ${serializedTx.toString('hex')}`);
+  const decodedTx = Transaction.fromRlpSerializedTx(serializedTx, { common });
 
-  const decodedTx = new Transaction(serializedTx);
-
-  t.ok(decodedTx.verifySignature(), 'Verify signature');
-  t.equal(decodedTx.from.toString('hex'), address, 'Address from signature');
+  t.ok(decodedTx.validate(), 'Valid signature');
+  t.equal(decodedTx.getSenderAddress().toString(), address, 'Address from signature');
 
   t.end();
 });
